383b87743ff9dc30872daecba9d07c58f12982c4
munipravy <munipravy@gmail.com>
2020-05-31 21:38:26 +0530

    change

--- run-app.sh
+++ run-app.sh
@@ -3,5 +3,4 @@ docker build -t app:1.0 -f Dockerfile .
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
-helm install counter-chart chart.yml
-
+helm install counterapp counter-chart
