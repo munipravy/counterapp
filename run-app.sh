4a8831b5500f67dc68f10df0e97f025108cce706
munipravy <munipravy@gmail.com>
2020-05-31 21:33:33 +0530

    Updated

--- run-app.sh
+++ run-app.sh
@@ -1,4 +1,7 @@
 mvn clean install
-docker build -t arc-gateway:1.0 -f deploy/Dockerfile .
-kubectl apply -f deploy/deploy-app.yml
-kubectl apply -f deploy/app-service.yml
+docker build -t app:1.0 -f Dockerfile .
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+helm install counter-chart chart.yml
+
