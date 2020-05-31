533bb34d9facf745325e559923e2f393cefee9a6
munipravy <munipravy@gmail.com>
2020-05-31 22:18:33 +0530

    Updated

--- run-kube.sh
+++ run-kube.sh
@@ -4,14 +4,14 @@ mvn clean install
 
 docker build -t app:1.0 -f Dockerfile .
 
-kubectl apply -f /home/pguluru/.counterapp/counterapp/helm/counter-chart/template/blue.yml
+kubectl apply -f helm/counterapptemplate/blue.yml
 
-kubectl apply -f /home/pguluru/.counterapp/counterapp/helm/counter-chart/template/green.yml
+kubectl apply -f helm/counter-chart/template/green.yml
 
-kubectl apply -f /home/pguluru/.counterapp/counterapp/helm/counter-chart/template/service.yml
+kubectl apply -f helm/counter-chart/template/service.yml
 
-kubectl apply -f /home/pguluru/.counterapp/counterapp/helm/counter-chart/template/Ingress-controller.yml
+kubectl apply -f helm/counter-chart/template/Ingress-controller.yml
 
-kubectl apply -f /home/pguluru/.counterapp/counterapp/helm/counter-chart/template/ingress.yml
+kubectl apply -f helm/counter-chart/template/ingress.yml
 
 
