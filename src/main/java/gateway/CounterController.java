41caee12c7dff04c44a4c0d6c33a89c8793f2d54
munipravy <munipravy@gmail.com>
2020-05-31 22:21:46 +0530

    Updated controller

--- src/main/java/gateway/CounterController.java
+++ src/main/java/gateway/CounterController.java
@@ -1,42 +1,45 @@
 package gateway;
 
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.concurrent.atomic.AtomicLong;
-
 
 @RestController
 @SpringBootApplication
 public class CounterController {
-
     private final AtomicLong counter = new AtomicLong();
-
+    @Autowired
+    private Environment env;
+    @GetMapping("/")
+    public String indexCounterApp() {
+        return  "Welcome to Counter App";
+    }
     @GetMapping("/counter")
     public String getCounter() {
         return counter.incrementAndGet() + "";
     }
-
     @PostMapping("/counter")
     public String postCounter() {
         counter.incrementAndGet();
         return counter.incrementAndGet()  + "";
     }
-
     @DeleteMapping("/counter")
     public String deleteCounter() {
         return counter.decrementAndGet() + "";
     }
-
     @GetMapping("/info")
     public String info() {
-        return "hello world";
+        StringBuilder sb = new StringBuilder();
+        sb.append("git commit hash: ").append("hashcode").append(System.getProperty("line.separator"));
+        sb.append("branch name of the source code: ").append("master").append(System.getProperty("line.separator"));
+        sb.append("environment name of the app: ").append(env.getProperty("app.name")).append(System.getProperty("line.separator"));
+        sb.append("hostname: ").append("localhost:8080").append(System.getProperty("line.separator"));
+        return sb.toString();
     }
-
     public static void main(String[] args) {
         SpringApplication.run(CounterController.class, args);
     }
-
 }
