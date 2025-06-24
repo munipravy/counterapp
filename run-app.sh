4ce3bf46acfc4bf18254224c66725dce767c5949
munipravy <munipravy@gmail.com>
2025-06-24 09:15:52 +0530

    Update run-app.sh

--- run-app.sh
+++ run-app.sh
@@ -1,6 +1,5 @@
 mvn clean install
 docker build -t app:1.0 -f Dockerfile .
-curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
 helm install counterapp counter-chart
